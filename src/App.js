e7fd8f1639a8297e5206d57ca64d7ac881a38596
박진산 <pap5608@naver.com>
2020-07-29 14:31:39 +0900

    클론코딩-chap4까지

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+function Food({name, picture, rating}) {
+  return (
+  <div>
+    <h2> I like {name} </h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+  );
+}
+
+const foodIlike = [
+  {
+    id : 1,
+    name : 'kimchi',
+    image : 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
+    rating : 3,
+  },
+  {
+    id : 2,
+    name : 'samgyopsal',
+    image : 'https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg',
+    rating : 5,
+  }
+];
+
+// function renderFood(dish) {
+//   return 
+//   <Food name={dish.name} picture={dish.image} />;
+// }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    {foodIlike.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+    ))}
     </div>
   );
+}
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number     // props 입력값 검사
 }
 
 export default App;
