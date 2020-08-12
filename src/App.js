eca6db69b124615621c05a1badd2e260d7d723cb
박진산 <pap5608@naver.com>
2020-08-12 14:51:59 +0900

    클론코딩 8장까지 마무리

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-function Food({name, picture, rating}) {
-  return (
-  <div>
-    <h2> I like {name} </h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-  );
-}
-
-const foodIlike = [
-  {
-    id : 1,
-    name : 'kimchi',
-    image : 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-    rating : 3,
-  },
-  {
-    id : 2,
-    name : 'samgyopsal',
-    image : 'https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg',
-    rating : 5,
-  }
-];
-
-// function renderFood(dish) {
-//   return 
-//   <Food name={dish.name} picture={dish.image} />;
-// }
+import Home from './routes/Home';
+import './App.css';
+import { HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 
 function App() {
   return (
-  <div>
-    {foodIlike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-    ))}
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/about" component={About} />        
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
   );
-}
-
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number     // props 입력값 검사
 }
 
 export default App;
